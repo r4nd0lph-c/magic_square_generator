5b983eade6ca8a109543cde3f202fdf1e7dfb774
Ксюха <60665381+r4nd0lph-c@users.noreply.github.com>
2022-09-28 22:18:19 +0300

    added a javadoc

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,32 +1,104 @@
 /**
- * Normal Magic Square
+ * A <b>Magic square</b> is a square array <i>n×n</i> filled with <i>n²</i>
+ * different numbers in such a way that the sum of the numbers in each row, each
+ * column and on both diagonals is the same.
+ * <p>
+ * If the array includes just the positive integers <i>1, 2, ..., n²</i> the
+ * magic square is said to be <b>normal</b>.
+ * <p>
+ * <ul>
+ * <li>{@code} Order {@code} is the number of integers along one side
+ * <i>n</i>;</li>
+ * <li>{@code} Magic constant {@code} is the sum of the numbers in each row,
+ * column
+ * and on the diagonals, depends only on <i>n</i> and is determined by the
+ * formula <i>n×(n²+1)/2</i>.</li>
+ * </ul>
+ * <p>
+ * Normal magic squares exist for all orders of <i>n>=1</i>, with the exception
+ * of <i>n=2</i>, although the case of <i>n=1</i> is trivial - the square
+ * consists of a single number.
+ * <p>
+ * This class is an implementation of a normal magic square.
+ * <p>
+ * <b>Examples:</b>
+ * <blockquote>
+ * 
+ * <pre>
+ * // Creating a class object (size n)
+ * MagicSquare square = new MagicSquare(n);
+ * 
+ * // Get the order value
+ * square.getOrder();
+ * 
+ * // Get the magic constant value
+ * square.getMagicConstant();
+ * 
+ * // Displaying the elements of the magic square on the screen
+ * for (int i = 0; i < square.getOrder(); i++) {
+ *     for (int j = 0; j < square.getOrder(); j++) {
+ *         System.out.print(square.getBody()[i][j] + " ");
+ *     }
+ *     System.out.println("");
+ * }
+ * </pre>
+ * 
+ * </blockquote>
  * 
  * @author r4nd0lph-c
  */
 public class MagicSquare {
+    /**
+     * This is the number of integers along one side <i>n</i>.
+     */
     protected int order;
+    /**
+     * This is the sum of the numbers in each row, column and on the diagonals. It
+     * depends only on <i>n</i> and is determined by the formula <i>n×(n²+1)/2</i>.
+     */
     protected int magic_constant;
+    /**
+     * This is a square matrix filled in according to the rules.
+     */
     protected int[][] body;
 
+    /**
+     * Constructor for the user.
+     * 
+     * @param o - the order of the magic square;
+     */
     public MagicSquare(int o) {
         Init(o, 1);
     }
 
+    /**
+     * Constructor for internal use.
+     * 
+     * @param o   - the order of the magic square;
+     * @param num the first number in the chain to generate for
+     *            {@link #SinglyEvenSolution()};
+     */
     private MagicSquare(int o, int num) {
         Init(o, num);
     }
 
+    /**
+     * Implementing the logic of {@link #MagicSquare(int)} and
+     * {@link #MagicSquare(int, int)}.
+     * 
+     * @param o   - the order of the magic square;
+     * @param num the first number in the chain to generate for
+     *            {@link #SinglyEvenSolution()};
+     */
     private void Init(int o, int num) {
         if (o < 1) {
-            // Error 1
-            throw new java.lang.Error("this is very bad 1");
+            throw new java.lang.Error("Normal magic squares exist for all orders of n>=1, with the exception of n=2.");
         } else if (o == 1) {
             this.order = o;
             this.magic_constant = 1;
             this.body = new int[][] { new int[] { 1 } };
         } else if (o == 2) {
-            // Error 2
-            throw new java.lang.Error("this is very bad 2");
+            throw new java.lang.Error("Normal magic squares exist for all orders of n>=1, with the exception of n=2.");
         } else {
             this.order = o;
             this.magic_constant = o * (o * o + 1) / 2;
@@ -41,12 +113,25 @@ public class MagicSquare {
         }
     }
 
+    /**
+     * Help function for {@link #SinglyEvenSolution()}
+     * 
+     * @param a - the first array;
+     * @param b - the second array;
+     * @param i - defines the position of the element (vertical);
+     * @param j - defines the position of the element (horizontal);
+     */
     private void Swap(int[][] a, int[][] b, int i, int j) {
-        a[i][j] = a[i][j] ^ b[i][j];
-        b[i][j] = a[i][j] ^ b[i][j];
-        a[i][j] = a[i][j] ^ b[i][j];
+        a[i][j] = a[i][j] ^ b[i][j] ^ (b[i][j] = a[i][j]);
     }
 
+    /**
+     * Help function for {@link #SinglyEvenSolution()}
+     * 
+     * @param block - the block whose elements will be inserted into the body;
+     * @param add_i - addition to the starting position (vertical);
+     * @param add_j - addition to the starting position (horizontal);
+     */
     private void Merge(int[][] block, int add_i, int add_j) {
         for (int i = add_i; i < block[0].length + add_i; i++) {
             for (int j = add_j; j < block[0].length + add_j; j++) {
@@ -55,6 +140,12 @@ public class MagicSquare {
         }
     }
 
+    /**
+     * Filling in the matrix according to the following algorithm:
+     * https://www.1728.org/magicsq1.htm
+     * 
+     * @param num - the first number in the chain to generate.
+     */
     protected void OddSolution(int num) {
         int completed = 1;
         int y = 0;
@@ -83,6 +174,10 @@ public class MagicSquare {
         }
     }
 
+    /**
+     * Filling in the matrix according to the following algorithm:
+     * https://www.1728.org/magicsq3.htm
+     */
     protected void SinglyEvenSolution() {
         // A B C D blocks generation
         int block_o = this.order / 2;
@@ -123,6 +218,10 @@ public class MagicSquare {
         Merge(blocks[3].getBody(), block_o, 0);
     }
 
+    /**
+     * Filling in the matrix according to the following algorithm:
+     * https://www.1728.org/magicsq2.htm
+     */
     protected void DoublyEvenSolution() {
         int num = 1;
         int t = this.order / 4;
@@ -139,14 +238,23 @@ public class MagicSquare {
         }
     }
 
+    /**
+     * @return {@link #order}
+     */
     public int getOrder() {
         return this.order;
     }
 
+    /**
+     * @return {@link #magic_constant}
+     */
     public int getMagicConstant() {
         return this.magic_constant;
     }
 
+    /**
+     * @return {@link #body}
+     */
     public int[][] getBody() {
         return this.body;
     }
